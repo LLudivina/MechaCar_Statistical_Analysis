0468d2d241205cc8be8bc63cc8a414edf09ef636
Ludivina LeMay <lemayludi@gmail.com>
2023-03-10 12:16:09 -0800

    adding part 2 and part 1 readme

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,4 +16,32 @@ model <- lm(mpg ~ vehicle_weight +
 #Use summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(model)
 
-#Part 2: Create Visualizations for the Trip Analysis
+# Part 2: Create Visualizations for the Trip Analysis
+#import and read file as a table
+
+#library(tidyverse)
+#data(Suspension_Coil, package = 'ggplot2')
+
+#read_csv = read.table('./Resources/Suspension_Coil.csv',sep=',')
+#print(read_csv)
+
+library(tidyverse)
+Suspension_Coil <- read.csv(file = "./Resources/Suspension_Coil.csv")
+View(Suspension_Coil)
+
+#use the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary_tibble <- Suspension_Coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+total_summary <- as.data.frame(total_summary_tibble)
+
+#view dataframe
+View(total_summary)
+
+#Use group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance,
+#and standard deviation of the suspension coil’s PSI column.
+lot_summary_tibble <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+lot_summary <- as.data.frame(lot_summary_tibble)
+
+#View lot_summary
+View(lot_summary)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+
+### *Figure 1*
+
+![Alt text](Resources/Part1_screenshot.png)
+
+In this case, both vehicle_length and ground_clearance provide a non-random amount of variance to the MPG values.  This is because both p-values are smaller than our assumed 0.05 percent significance level. Therefore, we would state that there is sufficient evidence to reject the null hypothesis and accept that there is a significant difference in mpg and a coefficient.
+
+The p-value of our regression analysis is 5.35e-11, which is smaller than an assumed significance level of 0.05%.  Therefore, it can be said that there is sufficient evidence to reject the null hypothesis, which means that the slope of the this linear model is not zero. 
+
+The model does not predit mpg of MechaCar prototypes effectively.
